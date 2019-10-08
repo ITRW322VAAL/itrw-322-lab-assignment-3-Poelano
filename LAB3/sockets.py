20cc0f0f9dd7b9434873ef5f0fd9571e3fcf43de
Poelano <poelanoserame@gmail.com>
2019-10-08 07:43:22 +0200

    26795809 - Python socket created

---
+++ LAB3/sockets.py
@@ -0,0 +1,12 @@
+import sockets
+import sys
+
+# Create socket using the following
+
+def sockets_create()
+    try:
+        global host
+        global port
+        global s
+        host = ''
+        port = 9999
